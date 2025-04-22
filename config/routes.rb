5306f5fd393674529f04a84bd3831d8a6035ba6b
Shuveksha Tuladhar <tuladhar.shuveksha@gmail.com>
2025-04-21 21:34:11 -0700

    Implement routes, controller, views and rspec for trimester and trimester details with id

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+
+  get "/trimesters", to: "trimesters#index"
+  get "/trimesters/:id", to: "trimesters#show"
+
 end
