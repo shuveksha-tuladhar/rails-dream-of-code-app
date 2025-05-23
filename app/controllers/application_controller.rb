9331d23fdb885702fa46720c261af178f404d509
Shuveksha Tuladhar <tuladhar.shuveksha@gmail.com>
2025-05-23 15:08:26 -0700

    Implement user role behavior and restricting routes based on user role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  
+  def require_admin
+    if session[:role] != 'admin'
+      flash[:alert] = 'You do not have access to that page'
+      redirect_to root_path
+    end
+  end
+  
   allow_browser versions: :modern
 end
