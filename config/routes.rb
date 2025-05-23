f7cfd852d293c53cbad0d63387f0816f0045c123
Shuveksha Tuladhar <tuladhar.shuveksha@gmail.com>
2025-05-23 14:15:40 -0700

    Implement login and session features

--- config/routes.rb
+++ config/routes.rb
@@ -31,4 +31,8 @@ Rails.application.routes.draw do
   get "/api/v1/courses", to: "courses#indexApi"
   get "/api/v1/courses/:id", to: "courses#showApi"
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
 end
